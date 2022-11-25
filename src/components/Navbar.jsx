6195f30931b85af5c7cc5ec5b925299485476e7c
adrianaperez123465 <112712237+adrianaperez123465@users.noreply.github.com>
2022-11-24 23:16:23 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,51 +43,74 @@ export default class card extends Component {
   }
 
   render() {
-    return (
-      <nav className="navbar bg-dark">
-        <div className="navbardiv">
-          <a className="navbar-brand">
-            <img className='pokeimg' src='https://assets.website-files.com/62c1627eee0defc3a1256898/62cf234679dbabe18fa50a1e_pokeapi_256%201.svg' alt='' width='220px' height='220px'></img>
-          </a>
-          <form onSubmit={this.handlerSubmit} className="input" role="search">
-            <input
-              className="inputtt"
-              type="search"
-              aria-label="Search"
-              placeholder="Ingrese el nombre del pokemón"
-              value={this.state.name}
-              onChange={this.handlerName}
-            ></input>
-            <br />
-            <br />
-            <button className="botoncito" type="submit">Search</button>
-            <br />
-            <br />
-            <div className="a-box">
-              <div className="img-container">
-                <div className="img-inner">
-                  <div className="inner-skew">
-                    <img src={this.state.img} alt="" />
+        if (this.state.img === "#") {
+      return (
+        <nav className="navbar bg-dark">
+          <div className="navbardiv">
+            <a className="navbar-brand">
+              <img className='pokeimg' src='https://assets.website-files.com/62c1627eee0defc3a1256898/62cf234679dbabe18fa50a1e_pokeapi_256%201.svg' alt='' width='220px' height='220px'></img>
+            </a>
+            <form onSubmit={this.handlerSubmit} className="input" role="search">
+              <input
+                className="inputtt"
+                type="search"
+                aria-label="Search"
+                placeholder="Ingrese el nombre del pokemón"
+                value={this.state.name}
+                onChange={this.handlerName}
+              ></input>
+              <br />
+              <br />
+              <button className="botoncito" type="submit">Search</button>
+              <br />
+              <br />
+            </form>
+          </div>
+        </nav>
+  
+      )
+    }else{
+      return (
+        <nav className="navbar bg-dark">
+          <div className="navbardiv">
+            <a className="navbar-brand">
+              <img className='pokeimg' src='https://assets.website-files.com/62c1627eee0defc3a1256898/62cf234679dbabe18fa50a1e_pokeapi_256%201.svg' alt='' width='220px' height='220px'></img>
+            </a>
+            <form onSubmit={this.handlerSubmit} className="input" role="search">
+              <input
+                className="inputtt"
+                type="search"
+                aria-label="Search"
+                placeholder="Ingrese el nombre del pokemón"
+                value={this.state.name}
+                onChange={this.handlerName}
+              ></input>
+              <br />
+              <br />
+              <button className="botoncito" type="submit">Search</button>
+              <br />
+              <br />
+              <div className="a-box">
+                <div className="img-container">
+                  <div className="img-inner">
+                    <div className="inner-skew">
+                      <img src={this.state.img} alt="" />
+                    </div>
+                  </div>
+                </div>
+                <div className="text-container">
+                  <h3>{this.state.name}</h3>
+                  <div>
+                    <p className="parrafo">Weight: {this.state.weight}Kg</p>
+                    <p className="parrafo">Height: {this.state.height}m</p>
+                    <p className="parrafo">Type:   {this.state.type}</p>
                   </div>
                 </div>
               </div>
-              <div className="text-container">
-                <h3>{this.state.name}</h3>
-                <div>
-                  <p className="parrafo">Weight: {this.state.weight}Kg</p>
-                  <p className="parrafo">Height: {this.state.height}m</p>
-                  <p className="parrafo">Type:   {this.state.type}</p>
-
-
-                </div>
-              </div>
-            </div>
-
-
-          </form>
-        </div>
-      </nav>
-
-    )
+            </form>
+          </div>
+        </nav>
+      )
+    }
   }
 }
